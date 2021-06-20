0df33fb7e71256b27a6d7335061521e12ac68ab9
Florian <alu.florian@gmail.com>
2021-06-20 20:06:21 +0200

    Init project

--- App.tsx
+++ App.tsx
@@ -2,23 +2,16 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import BottomTabNavigator from './navigation/BottomTabNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+        <NavigationContainer>
+            <SafeAreaProvider>
+                <StatusBar />
+                <BottomTabNavigator />
+            </SafeAreaProvider>
+        </NavigationContainer>
     );
-  }
 }
