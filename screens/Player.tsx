494dd3bbf4fdb7f299aa89887b931be5af3947c1
Florian <alu.florian@gmail.com>
2021-07-02 21:48:48 +0200

    fix: Add SafeAreaView to Player screen

--- screens/Player.tsx
+++ screens/Player.tsx
@@ -1,8 +1,13 @@
 import React, { FC } from 'react';
 import AudioPlayer from '../components/AudioPlayer';
+import { SafeAreaView } from 'react-native';
 
 const Home: FC = () => {
-    return <AudioPlayer mp3={'https://cause-commune.fm/avv/29-AVV-Paul%20Citron-Ainsi%20va%20la%20ville.mp3'} />;
+    return (
+        <SafeAreaView>
+            <AudioPlayer mp3={'https://cause-commune.fm/avv/29-AVV-Paul%20Citron-Ainsi%20va%20la%20ville.mp3'} />
+        </SafeAreaView>
+    );
 };
 
 export default Home;
