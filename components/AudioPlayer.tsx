a5d515a2e51fdd49e44e93a7bc75075bdc41aa39
Florian <alu.florian@gmail.com>
2021-07-06 19:29:06 +0200

    chore: Add fake title and image

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 
-import { StyleSheet, View } from 'react-native';
+import { StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
 import { IconButton } from 'react-native-paper';
 import Slider from '@react-native-community/slider';
 import useAudioPlayer from '../hooks/useAudioPlayer';
@@ -9,7 +9,7 @@ import TimeCode from './TimeCode';
 
 const FORWARD_AND_REWIND_MS = 10000;
 
-const AudioPlayer: FC<AudioPlayerProps> = ({ mp3 }) => {
+const AudioPlayer: FC<AudioPlayerProps> = ({ mp3, style }) => {
     const theme = useTheme();
     const {
         playbackStatus,
@@ -22,22 +22,27 @@ const AudioPlayer: FC<AudioPlayerProps> = ({ mp3 }) => {
     } = useAudioPlayer(mp3, FORWARD_AND_REWIND_MS);
 
     return (
-        <View style={styles.root}>
-            <TimeCode
-                current={playbackStatus?.positionMillis}
-                max={playbackStatus?.durationMillis}
-                style={styles.timeCode}
-            />
-            <Slider
-                minimumValue={0}
-                maximumValue={playbackStatus?.durationMillis || 1000}
-                value={playbackStatus?.positionMillis || 0}
-                onSlidingStart={onSlidingStart}
-                onSlidingComplete={onSlidingComplete}
-                minimumTrackTintColor={theme.colors.iconFocused}
-                maximumTrackTintColor={theme.colors.slider}
-                thumbTintColor={theme.colors.primary}
-            />
+        <View style={[styles.root, style]}>
+            <View>
+                <TimeCode
+                    current={playbackStatus?.positionMillis}
+                    max={playbackStatus?.durationMillis}
+                    style={styles.timeCode}
+                />
+                <View style={styles.slider}>
+                    <Slider
+                        minimumValue={0}
+                        maximumValue={playbackStatus?.durationMillis || 1000}
+                        value={playbackStatus?.positionMillis || 0}
+                        onSlidingStart={onSlidingStart}
+                        onSlidingComplete={onSlidingComplete}
+                        minimumTrackTintColor={theme.colors.iconFocused}
+                        maximumTrackTintColor={theme.colors.slider}
+                        thumbTintColor={theme.colors.primary}
+                        style={styles.slider}
+                    />
+                </View>
+            </View>
             <View style={styles.controls}>
                 <IconButton icon='rewind-10' onPress={rewind} color={theme.colors.audioPlayerControl} size={32} />
                 <IconButton
@@ -59,6 +64,7 @@ const AudioPlayer: FC<AudioPlayerProps> = ({ mp3 }) => {
 
 interface AudioPlayerProps {
     mp3: string;
+    style?: StyleProp<ViewStyle>;
 }
 
 const styles = StyleSheet.create({
@@ -70,10 +76,12 @@ const styles = StyleSheet.create({
         marginBottom: 12,
     },
     controls: {
-        display: 'flex',
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    slider: {
+        height: 20,
     },
 });
 export default AudioPlayer;
--- screens/Player.tsx
+++ screens/Player.tsx
@@ -1,13 +1,55 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import AudioPlayer from '../components/AudioPlayer';
-import { SafeAreaView } from 'react-native';
+import { Image, SafeAreaView, StyleSheet } from 'react-native';
+import { Title } from 'react-native-paper';
 
-const Home: FC = () => {
+const Player: FC = () => {
+    // Testing a way to maintain aspect ratio on images without knowing its dimensions in advance
+    const [aspectRatio, setAspectRatio] = useState(1);
+    useEffect(() => {
+        Image.getSize(FAKE_DATA.image, (width, height) => {
+            console.log({
+                width,
+                height,
+                aspectRatio: width / height,
+            });
+            setAspectRatio(width / height);
+        });
+    }, [FAKE_DATA.image]);
+
     return (
-        <SafeAreaView>
-            <AudioPlayer mp3={'https://cause-commune.fm/avv/29-AVV-Paul%20Citron-Ainsi%20va%20la%20ville.mp3'} />
+        <SafeAreaView style={styles.root}>
+            <Image source={{ uri: FAKE_DATA.image }} style={[styles.image, { aspectRatio }]} />
+            <Title style={styles.title}>{FAKE_DATA.title}</Title>
+            <AudioPlayer mp3={FAKE_DATA.mp3} style={styles.audioPlayer} />
         </SafeAreaView>
     );
 };
 
-export default Home;
+const styles = StyleSheet.create({
+    image: {
+        flex: 1,
+        resizeMode: 'contain',
+        margin: 10,
+    },
+    root: {
+        flex: 1,
+        margin: 10,
+    },
+    title: {
+        textAlign: 'center',
+        marginTop: 40,
+    },
+    audioPlayer: {
+        flex: 1,
+        justifyContent: 'space-evenly',
+    },
+});
+
+const FAKE_DATA = {
+    image: 'https://cause-commune.fm/wp-content/uploads/2019/11/avv-2.jpg',
+    title: 'Ainsi va la ville',
+    mp3: 'https://cause-commune.fm/avv/29-AVV-Paul%20Citron-Ainsi%20va%20la%20ville.mp3',
+};
+
+export default Player;
