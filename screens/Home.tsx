521fbf042cd2409ae723c41a42f38b021d3a84a3
fortin-p <fortin.p@hotmail.fr>
2021-07-07 22:49:22 +0200

    TMP fetch api

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -3,14 +3,17 @@ import { ScrollView } from 'react-native';
 import HomeTitle from '../components/homepage/HomeTitle';
 import HomeCardCarousel from '../components/homepage/HomeCardCarousel';
 import PodcastComponent from '../components/homepage/PodcastComponent';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
+const queryClient = new QueryClient();
 const Home: FC = () => {
     return (
-        <ScrollView style={{ flex: 1, backgroundColor: '#FEF7F9' }}>
-            <HomeTitle />
-            <HomeCardCarousel />
-            <PodcastComponent />
-        </ScrollView>
+        <QueryClientProvider client={queryClient}>
+            <ScrollView style={{ flex: 1, backgroundColor: '#FEF7F9' }}>
+                <HomeTitle />
+                <HomeCardCarousel />
+                <PodcastComponent />
+            </ScrollView>
+        </QueryClientProvider>
     );
 };
 
