12b1344039b7bc08eabb8b0dff55f45d3338efe5
Florian <alu.florian@gmail.com>
2021-07-29 09:57:19 +0200

    feat: on podcast press now plays the podcast instead of displaying an alert

--- components/homepage/ListPodcast.tsx
+++ components/homepage/ListPodcast.tsx
@@ -1,18 +1,30 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useMemo } from 'react';
 import { Card, Title } from 'react-native-paper';
 import { Dimensions, Image, StyleSheet, View } from 'react-native';
 import { decode } from 'html-entities';
 import { Podcast } from '../../types/Podcast';
+import { useNavigation } from '@react-navigation/native';
+import useAudioPlayer from '../../hooks/useAudioPlayer';
 
 const ListPodcast: FC<ListPodcastProps> = ({ item }) => {
+    const navigation = useNavigation();
+    const { stop } = useAudioPlayer();
+    const playerParams = useMemo(
+        () => ({
+            mp3: item.meta.audio_file,
+            title: decode(item.title.rendered),
+            image: item.episode_player_image,
+        }),
+        [item]
+    );
+    const onPress = useCallback(async () => {
+        await stop();
+        navigation.navigate('Player', playerParams);
+    }, [stop]);
+
     return (
         <View>
-            <Card
-                style={styles.card}
-                onPress={() => {
-                    alert('You tapped the button!');
-                }}
-            >
+            <Card style={styles.card} onPress={onPress}>
                 <Image source={{ uri: item.episode_player_image }} style={styles.image} />
                 <Card.Content>
                     <Title>{decode(item.title.rendered)}</Title>
