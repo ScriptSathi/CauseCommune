eaa95252681bbf7bfdbef2ffd187c1bd3e2b5724
fortin-p <fortin.p@hotmail.fr>
2021-08-31 21:59:38 +0200

    loader too long

--- screens/Player.tsx
+++ screens/Player.tsx
@@ -1,6 +1,6 @@
-import React, { FC, useMemo } from 'react';
+import React, { FC, useMemo, useState } from 'react';
 import AudioPlayer from '../components/player/AudioPlayer';
-import { Image, SafeAreaView, StyleSheet, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { ActivityIndicator, Title } from 'react-native-paper';
 import { useRoute } from '@react-navigation/native';
 import useAudioPlayer from '../hooks/useAudioPlayer';
@@ -9,8 +9,11 @@ import { useQuery } from 'react-query';
 import getPlayerArguments from '../fns/getPlayerArguments';
 import getPlayerPodcast from '../queries/getPlayerPodcast.query';
 
+const errorLoadingMessage =  "Podcast indisponible";
+
 const Player: FC = () => {
     // Testing a way to maintain aspect ratio on images without knowing its dimensions in advance
+    const [animating , setAnimating] = useState(true)
     const { data: podcasts } = useQuery('/player', getPlayerPodcast);
     const route = useRoute();
     const { mp3, title, image } = useMemo(() => {
@@ -27,13 +30,20 @@ const Player: FC = () => {
     }, [route.params]);
 
     const { isLoading } = useAudioPlayer();
-
+    let closeActivityIndicator = () => setTimeout(() => setAnimating(false), 10000)
+    closeActivityIndicator()
     return (
         <SafeAreaView style={styles.root}>
             <View style={styles.imageContainer}>
                 <Image source={{ uri: image }} style={[styles.image]} />
-                {isLoading && <ActivityIndicator />}
+                {isLoading && <ActivityIndicator animating={animating} />}
+                {!animating &&
+                <Text style={styles.errorMessage}>
+                    {errorLoadingMessage}
+                </Text>
+                }
             </View>
+
             <Title style={styles.title}>{title}</Title>
             <AudioPlayer mp3={mp3} style={styles.audioPlayer} />
         </SafeAreaView>
@@ -66,6 +76,10 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'space-evenly',
     },
+    errorMessage: {
+        marginBottom: 5,
+        color: '#E73059',
+    }
 });
 
 export default Player;
