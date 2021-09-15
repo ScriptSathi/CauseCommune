79a8e23cb2f6eeb7e3e6286e1aea9cc64bd02088
Asmoth <contact@paroledemetaleux.com>
2021-09-15 13:18:08 +0200

    tentative to fix infinite scroll kick bug when first time we play a podcast fix other bug

--- components/emission/EmissionCardPodcast.tsx
+++ components/emission/EmissionCardPodcast.tsx
@@ -1,23 +1,32 @@
 import * as React from 'react';
 import { FC } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import {FlatList, Platform, StyleSheet, View} from 'react-native';
 import { Podcast } from '../../types/Podcast';
 import PodcastCard from './PodcastCard';
 
 const EmissionCardPodcast: FC<EmissionCardPodcastProps> = ({ podcasts = [], podcastTitle, isLoading, fetchNextPage }) => {
+    // @ts-ignore
+    const renderItemFunction = ({item}) => {
+        return (
+            <PodcastCard podcast={item} podcastTitle={podcastTitle} />
+        );
+    }
+
     return (
         <View style={styles.container}>
             <FlatList
                 refreshing={isLoading}
                 data={podcasts}
                 horizontal
+                initialNumToRender={5}
                 showsHorizontalScrollIndicator={false}
                 bounces={false}
+                removeClippedSubviews={Platform.OS==='android'}
                 scrollEventThrottle={16}
                 keyExtractor={item => item.id.toString()}
                 onEndReachedThreshold={1}
                 onEndReached={() => fetchNextPage()}
-                renderItem={({ item }) => <PodcastCard podcast={item} podcastTitle={podcastTitle} />}
+                renderItem={renderItemFunction}
             />
         </View>
     );
